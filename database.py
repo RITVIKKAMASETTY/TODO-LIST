23cc326198b7663ef4e7399f2046f9b035217f96
RITVIKKAMASETTY <ritvikksymp@gmail.com>
2025-06-23 15:18:50 +0530

    add

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-SQLALCHEMY_DATABASE_URL = 'sqlite:///todolist.db'
-engine=create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":False})
+import os
+from dotenv import load_dotenv
+load_dotenv(".env")
+SQLALCHEMY_DATABASE_URL =os.getenv("POSTGRESURL")
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
--- routers/users.py
+++ routers/users.py
@@ -1,42 +1,58 @@
-from fastapi import FastAPI,APIRouter,Path,status,HTTPException,Depends
+from fastapi import APIRouter, status, HTTPException, Depends
 import models
-from database import engine,SessionLocal
+from database import SessionLocal
 from sqlalchemy.orm import Session
 from typing import Annotated
-from models import Todo
-from pydantic import BaseModel,Field
+from sqlalchemy.exc import IntegrityError
+from pydantic import BaseModel, Field
 from routers.auth import getcurrentuser
 from routers.auth import bcryot_context
+
+
 class Userverification(BaseModel):
-    password:str
-    newpassword:str=Field(min_length=4)
+    password: str
+    newpassword: str = Field(min_length=4)
+
+
 def get_db():
-    db=SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-router=APIRouter(
-    prefix="/user",
-    tags=["user"]
-)
-user_depends=Annotated[dict,Depends(getcurrentuser)]
-db_depends=Annotated[Session,Depends(get_db)]
-@router.get("/getuser",status_code=status.HTTP_200_OK)
-async def get_user(db:db_depends,user:user_depends):
-    user=db.query(models.User).filter(models.User.id==user["id"]).first()
+
+
+router = APIRouter(prefix="/user", tags=["user"])
+user_depends = Annotated[dict, Depends(getcurrentuser)]
+db_depends = Annotated[Session, Depends(get_db)]
+
+
+@router.get("/getuser", status_code=status.HTTP_200_OK)
+async def get_user(db: db_depends, user: user_depends):
+    user = db.query(models.User).filter(models.User.id == user["id"]).first()
     if user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Unauthorized")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
+        )
     return user
-@router.put("/changepassword",status_code=status.HTTP_204_NO_CONTENT)
-async def change_password(user:user_depends,db:db_depends,userverification:Userverification):
+
+
+@router.put("/changepassword", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(
+    user: user_depends, db: db_depends, userverification: Userverification
+):
     if user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Unauthorized")
-    user=db.query(models.User).filter(models.User.id==user["id"]).first()
-    user.hashedpassword=bcryot_context.hash(userverification.newpassword)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
+        )
+    user = db.query(models.User).filter(models.User.id == user["id"]).first()
+    user.hashedpassword = bcryot_context.hash(userverification.newpassword)
     try:
         db.commit()
-    except IntegrityError as e:
+    except IntegrityError:
         db.rollback()
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Error updating user")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error updating user",
+        )
     return user
